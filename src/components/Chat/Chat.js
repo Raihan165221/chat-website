eabcca478c51197cf90e0ac22bcf0f143bd4f813
Raihan Uddin <raihan165221@gmail.com>
2022-12-25 12:27:27 +0600

    added chat features

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,22 +1,36 @@
 import React, { useRef, useState } from 'react';
 import './Chat.css';
 const Chat = () => {
-    const [message, setMessage] = useState('');
+    const [message, setMessage] = useState([]);
     const myRef = useRef();
-
-    const handleMessage = () => {
-        setMessage(myRef.current.value);
+    const anotherRef = useRef()
+    const handleMessage = (event) => {
+        const myText = myRef.current.value;
+        const anotherText = anotherRef.current.value;
+        myRef.current.value = '';
+        anotherRef.current.value = '';
+        setMessage([...message, {
+            firstMessage: myText,
+            secondMessage: anotherText
+        }]);
     }
     return (
         <div className='chat-container'>
             <div className='my-text'>
-                <p>{message}</p>
+                <div>
+                    {
+                        message.map(msg => <div>
+                            <p className='first-message'>{msg.firstMessage}</p>
+                            <p className='second-message'>{msg.secondMessage}</p>
+                        </div>)
+                    }
+                </div>
                 <input ref={myRef} type="text" placeholder='Type a message' />
-                <button onClick={handleMessage} className='my-btn'>Send</button>
+                <button onClick={() => handleMessage(event)} className='my-btn'>Send</button>
             </div>
             <div className='another-text'>
-                <input type="text" placeholder='Type a message' />
-                <button onClick={handleMessage} className='another-btn'>Send</button>
+                <input ref={anotherRef} type="text" placeholder='Type a message' />
+                <button onClick={() => handleMessage(event)} className='another-btn'>Send</button>
             </div>
         </div>
     );
